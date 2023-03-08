d18fe4cad025818802c1acaa298cdd3668754d48
Gokil <76507378+googolgokil@users.noreply.github.com>
2023-03-08 19:15:04 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num_1;
+    int num_2;
+    int Total;
+    printf("Enter the Number");
+    scanf("%d%d",&num_1,&num_2);
+    Total=num_1+num_2;
+    printf("Total is : %d", Total);
+    return 0;
+}
