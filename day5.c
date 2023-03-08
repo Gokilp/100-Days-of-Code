1dfffc957d8cbe7255cf06c3258048c0b60a315c
Gokil <76507378+googolgokil@users.noreply.github.com>
2023-03-08 22:21:03 +0530

    Add files via upload

---
+++ day5.c
@@ -0,0 +1,17 @@
+// Find Area of Traingle
+#include<stdio.h>
+int main(void)
+{
+  float  base,height,area;
+    printf("Enter the base");
+    scanf("%f",&base);
+
+    printf("Enter the Height");
+    scanf("%f",&height);
+    area=0.5*base*height;
+    printf("Area of Traingle is  %f",area);
+    return 0;
+}
+
+
+
