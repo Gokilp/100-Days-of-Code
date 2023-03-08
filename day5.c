6e39098b237715b4702250377735e39114425e10
Gokil <76507378+googolgokil@users.noreply.github.com>
2023-03-08 22:17:14 +0530

    Add files via upload

---
+++ day5.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main(void)
+{
+  float  base,height,area;
+    printf("Enter the base");
+    scanf("%f",&base);
+
+    printf("Enter the Height");
+    scanf("%f",&height);
+    area=0.5*base*height;
+    printf("area of Traingle is  %f",area);
+
+
+}
+
+
+
